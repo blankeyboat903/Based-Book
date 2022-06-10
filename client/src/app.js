5422facf2766be88b0b25f14f718b932df2c8071
Hugo530 <101437827+Hugo530@users.noreply.github.com>
2022-06-10 12:29:06 -0700

    added starter code for app.js in cliend folder

--- client/src/app.js
+++ client/src/app.js
@@ -1,3 +1,16 @@
+// todo: imported from activity 18 unit 22
+// import React from 'react';
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+// import { BrowserRouter as Router, Routes, Route } from 
+// 'react-router-dom';
+
+
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser');
@@ -6,6 +19,37 @@ var mongoose = require('mongoose')
 var fs = require('fs');
 var path = require('path');
 require('dotenv/config');
+
+// todo: imported from activity 18 unit 22
+// import Home from './pages/Home';
+// import Signup from './pages/Signup';
+// import Login from './pages/Login';
+// import Headers from './components/Header';
+// import Footer from './components/Footer';
+
+// // Construct our main GraphQL API endpoint
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
+
+// // Construct request middleware that will attach the JWT token to every request as an `authorization` header
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem('id_token');
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+
+// const client = new ApolloClient({
+//   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
 
 
 mongoose.connect(process.env.MONGO_URL,
