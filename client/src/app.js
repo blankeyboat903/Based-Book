36610df17b46854f6111f385b07710ae304f6427
Jonathan Edwards <jondedwards13@gmail.com>
2022-06-11 13:55:48 -0500

    Fixed various issues in client and got server running blank page by commenting out Bucket List.

--- client/src/app.js
+++ client/src/app.js
@@ -1,70 +1,11 @@
-// todo: imported from activity 18 unit 22
-// import React from 'react';
-// import {
-//   ApolloClient,
-//   InMemoryCache,
-//   ApolloProvider,
-//   createHttpLink,
-// } from '@apollo/client';
-// import { setContext } from '@apollo/client/link/context';
-// import { BrowserRouter as Router, Routes, Route } from 
-// 'react-router-dom';
-
-
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose')
-  
-var fs = require('fs');
-var path = require('path');
-require('dotenv/config');
-
-// todo: imported from activity 18 unit 22
-// import Home from './pages/Home';
-// import Signup from './pages/Signup';
-// import Login from './pages/Login';
-// import Headers from './components/Header';
-// import Footer from './components/Footer';
-
-// // Construct our main GraphQL API endpoint
-// const httpLink = createHttpLink({
-//   uri: '/graphql',
-// });
-
-// // Construct request middleware that will attach the JWT token to every request as an `authorization` header
-// const authLink = setContext((_, { headers }) => {
-//   // get the authentication token from local storage if it exists
-//   const token = localStorage.getItem('id_token');
-//   // return the headers to the context so httpLink can read them
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
-
-// const client = new ApolloClient({
-//   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
-
-
-mongoose.connect(process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true }, err => {
-      console.log('connected')
-  });
+import './app.css';
+// import BucketList from './components/BucketList';
 
 function App() {
   return (
-    <CloudinaryContext cloudName="girthycloud">
-  <div>
-    <Image publicId="sample" width="50" />
-  </div>
-  <Image publicId="sample" width="0.5" />
-</CloudinaryContext>
+    <div className="bucket-app">
+      {/* <BucketList /> */}
+    </div>
   );
 }
 
