de1384aa9f1e827e8111e1481c1a921f888ec55c
Jonathan Edwards <jondedwards13@gmail.com>
2022-06-12 13:52:20 -0500

    Got the Home page running and fixed errors with starting the server

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,33 +1,33 @@
 import React, {useState} from 'react';
-import './style.css'
+// import './style.css'
 
 function CreateUser() {
     return(
       <div className="form">
           <div className="form-body">
               <div className="username">
-                  <label className="form__label" for="firstName">First Name </label>
+                  <label className="form__label" htmlFor="firstName">First Name </label>
                   <input className="form__input" type="text" id="firstName" placeholder="First Name"/>
               </div>
               <div className="lastname">
-                  <label className="form__label" for="lastName">Last Name </label>
+                  <label className="form__label" htmlFor="lastName">Last Name </label>
                   <input  type="text" name="" id="lastName"  className="form__input"placeholder="LastName"/>
               </div>
               <div className="email">
-                  <label className="form__label" for="email">Email </label>
+                  <label className="form__label" htmlFor="email">Email </label>
                   <input  type="email" id="email" className="form__input" placeholder="Email"/>
               </div>
               <div className="password">
-                  <label className="form__label" for="password">Password </label>
+                  <label className="form__label" htmlFor="password">Password </label>
                   <input className="form__input" type="password"  id="password" placeholder="Password"/>
               </div>
               <div className="confirm-password">
-                  <label className="form__label" for="confirmPassword">Confirm Password </label>
+                  <label className="form__label" htmlFor="confirmPassword">Confirm Password </label>
                   <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password"/>
               </div>
           </div>
-          <div class="footer">
-              <button type="submit" class="btn">Register</button>
+          <div className="footer">
+              <button type="submit" className="btn">Register</button>
           </div>
       </div>      
     )       
