0cc0eba8a3b1b1f6bd0834000335e9e606b4a0fd
Cody Claussen <codyclaussen@Codys-MacBook-Pro.local>
2022-06-12 21:24:24 -0700

    changes

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,12 +3,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Auth from '../../utils/auth';
+import auth from '../../utils/auth';
 
 const Header = () => {
   const logout = (event) => {
     event.preventDefault();
-    Auth.logout();
+    auth.logout();
   };
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
