1790dce356ac9bb30d5466d7c82ebef5aebb545b
Alex Blankenship <blankeyboat@gmail.com>
2022-06-12 22:56:24 -0500

    auth fixed

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -43,7 +43,7 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 
-import Auth from '../utils/Auth';
+import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({
